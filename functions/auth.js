4da5227b8078f6b5a281e07fe66df418897d0e8e
Michael Mainguy <mike.mainguy@gmail.com>
2024-08-14 17:43:29 -0500

    added worker callback function for workos auth.

--- functions/auth.js
+++ functions/auth.js
@@ -1,10 +1,13 @@
 const workos = new WorkOS(env.WORKOS_SECRET);
-const org = 'org_01J58ZZ8RPA6AF3B6717PHSVA8';
-export function onRequest(context) {
-    const url = workos.userManagement.getAuthorizationUrl({
-        provider: 'authkit',
-        redirectUri: 'https://www.cybersecshield.com/callback',
-        clientId
-    })
-    return Response.redirect(url, 301);
+const organization = 'org_01J58ZZ8RPA6AF3B6717PHSVA8';
+const clientId = env.WORKOS_CLIENTID;
+const auth = async (context) => {
+      const url = workos.sso.getAuthorizationUrl({
+          organization,
+          provider: 'authkit',
+          redirectUri: 'https://www.cybersecshield.com/callback',
+          clientId
+      })
+      return Response.redirect(url, 301);
 }
+export const onRequest = [auth];
