7f3c17e7d620cafd5b1813bb3c81ec4bceb7e3a4
Michael Mainguy <mike.mainguy@gmail.com>
2024-08-14 18:00:51 -0500

    added worker callback function for workos auth.

--- functions/auth.js
+++ functions/auth.js
@@ -1,8 +1,10 @@
 import WorkOS from '@workos-inc/node';
-const workos = new WorkOS(env.WORKOS_SECRET);
+
 const organization = 'org_01J58ZZ8RPA6AF3B6717PHSVA8';
-const clientId = env.WORKOS_CLIENTID;
-const auth = async (context) => {
+
+const auth = async (context, env) => {
+    const workos = new WorkOS(env.WORKOS_SECRET);
+    const clientId = env.WORKOS_CLIENTID;
       const url = workos.sso.getAuthorizationUrl({
           organization,
           provider: 'authkit',
