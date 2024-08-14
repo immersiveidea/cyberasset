34be23c48cc0457eb1612e5d0c653ea7fa8c46a4
Michael Mainguy <mike.mainguy@gmail.com>
2024-08-14 18:06:26 -0500

    added worker back function for workos auth.

--- functions/callback.js
+++ functions/callback.js
@@ -2,7 +2,7 @@ import WorkOS from '@workos-inc/node';
 
 const organization = 'org_01J58ZZ8RPA6AF3B6717PHSVA8';
 
-const auth = async (context) => {
+const auth = async (context, env) => {
     const workos = new WorkOS(env.WORKOS_SECRET);
     const clientId = env.WORKOS_CLIENTID;
     const url = workos.sso.getAuthorizationUrl({
